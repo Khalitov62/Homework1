8ed746f7dd7396ac2dc2cc7fca88cd60657d9655
Khalitov62 <tu7m5rk73code.edu.az>
2022-03-24 13:59:38 +0400

    added

---
+++ ConsoleApp21/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp21
+{
+    internal static class Extensions
+    {
+        public static bool customContains(this string word, string searched)
+        {
+            searched = searched.ToLower();
+            int i = 0;
+            foreach (char c in word.ToLower())
+            {
+                if (i != searched.Length)
+                {
+                    if (c == searched[i])
+                    {
+                        i++;
+                    }
+                }
+                else if (c == searched[i])
+                {
+                    i = 0;
+                }
+                break;
+            }
+            if (i == searched.Length)
+            {
+                return true;
+
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cumle daxil edin");
+            string word = Console.ReadLine();
+            Console.WriteLine("Axtardiginiz sozu  daxil adin");
+            string searched = Console.ReadLine();
+            bool answer = word.customContains(searched);
+            if (answer == true) Console.WriteLine("Soz var");
+            else Console.WriteLine("Soz yoxdur");
+        }
+    }
+}
